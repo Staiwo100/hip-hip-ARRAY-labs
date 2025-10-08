b79904d29fe3086e97b07ba0b93d3cd0657a6144
Staiwo100 <sotaiwo@my.waketech.edu>
2025-10-08 09:39:54 -0400

    Add files via upload

---
+++ ShufflePlaylist.java
@@ -0,0 +1,47 @@
+/*
+Name: Samuel Taiwo
+Date: 10/8/2025
+Description:  (The program takes a playlist in the form of a string array and shuffles it every time the program runs.)
+*/
+
+import java.util.Scanner;
+
+public class ShufflePlaylist {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static String getInput(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+
+    public static int getIntInput(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextInt();
+    }
+    public static void main(String[] args){
+        String[] Playlist = {
+                "Shake It Off – Taylor Swift",
+                "I Love You – Barney",
+                "Baby Shark – Pinkfong",
+                "Never Gonna Give You Up – Rick Astley",
+                "Axel F (Crazy Frog) – Crazy Frog",
+                "Gangnam Style – PSY",
+                "What Does the Fox Say? – Ylvis"
+        };
+
+        shuffle(Playlist);
+        System.out.println("Shuffled Playlist: ");
+        for (String song : Playlist){
+            System.out.println(song);
+        }
+    }
+    public static void shuffle(String[] Playlist){
+        for (int i = 0; i<Playlist.length; i++){
+            int k = (int)(Math.random()*(i+1));
+            String temporary = Playlist[i];
+            Playlist[i]=Playlist[k];
+            Playlist[k]=temporary;
+
+        }
+    }
+}
